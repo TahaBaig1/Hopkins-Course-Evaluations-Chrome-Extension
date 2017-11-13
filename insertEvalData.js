dff8a09344ddf22a8661671e0bc5202b0297b0b4
Taha Baig <tahab27@gmail.com>
2017-11-12 19:32:55 -0500

    Implemented making AJAX requests to API and obtaining data

--- insertEvalData.js
+++ insertEvalData.js
@@ -2,12 +2,11 @@ $("document").ready( () => {
 	insertEvalData();
 });
 
-
 function insertEvalData() {
 	//determine URL of course selection site before appending data
-	chrome.runtime.sendMessage( {"action": "getTabURL"}, (url) => {
+	chrome.runtime.sendMessage({"action": "getTabURL"}, (url) => {
 		if (url.startsWith("https://sis.jhu.edu/sswf/SSS/SearchForClasses/SSS_ClassResults.aspx")) {
-			//find location of course number and instructor column
+			//find location of course number and instructor column on course selection table
 			var numCol = -1;
 			var profCol = -1;
 
@@ -20,26 +19,42 @@ function insertEvalData() {
 				}
 
 				if (numCol != -1 && profCol != -1) return false; //break out of loop
-				else									 count++;
+				else							   count++;
 			});
 
-			//if columns are not found, then there is no where to insert data, exit
+			//if locations are not found, then there is no where to insert data, exit
 			if (numCol == -1 || profCol == -1) return;
 
 			//appending data 
 			$("#results .odd").add(".even").each(function() {
 				var $cols = $(this).children("td");
-
+				
+				//appending data in courses column
 				var numElem = $cols.get(numCol);
-				var number = numElem.innerText;
+				var numberQuery = { "number": numElem.innerText };
+				requestData(numberQuery, numElem, appendData);
 
+				//appending data in professors column
 				var profElem = $cols.get(profCol);
-				var professor = profElem.innerText;
-
-				console.log(number + " " + professor);
+				var professorQuery = { "number": numElem.innerText, "professor": profElem.innerText };
+				requestData(professorQuery, numElem, appendData);
 	 		});
 
 		}
 	});
 }
+
+function appendData(elem, coursesData) {
+	//TODO: Create function that appends data returned from API into HTML of page
+}
+
+//Makes AJAX request to API to collect evaluations data 
+//appends data to page utilizing passed in element and callback function
+function requestData(query, elem, callback) {
+	chrome.runtime.sendMessage({"action": "getCoursesData", "coursesQuery": query}, (data) => {
+		callback(elem, data);
+	});
+}
+
+
 
--- requests.js
+++ requests.js
@@ -3,6 +3,18 @@ chrome.runtime.onMessage.addListener((request, sender, callback) => {
 		chrome.tabs.query( {currentWindow: true, active: true}, (tabs) => {
 			callback(tabs[0].url);
 		});
+	} else if (request.action == "getCoursesData") {
+		//gets JSON data of course evaluations via request to API
+		var params = $.param(request.coursesQuery)
+		var requestURL = "https://jhu-course-evaluations.herokuapp.com/courses?" + params;
+		console.log(requestURL);
+		
+		$.ajax({
+			url: requestURL,
+			dataType: "json"
+		}).done( (data) => {
+			callback(data);
+		});
 	}
 
 	return true; //to make listener asynchronous
